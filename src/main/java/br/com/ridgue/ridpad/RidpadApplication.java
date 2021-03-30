5ebab73153c1405ef800f19f77666e79553b6fce
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-30 17:29:35 -0300

    Initialize project

---
+++ src/main/java/br/com/ridgue/ridpad/RidpadApplication.java
@@ -0,0 +1,11 @@
+package br.com.ridgue.ridpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidpadApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RidpadApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/ridgue/ridpad/RidpadApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ridgue.ridpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
