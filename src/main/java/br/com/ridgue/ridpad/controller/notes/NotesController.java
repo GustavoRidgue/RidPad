52ff25f3af4b2dcd3e244ee3802fb322ad9b343b
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-31 17:57:47 -0300

    Create alter note page

--- src/main/java/br/com/ridgue/ridpad/controller/notes/NotesController.java
+++ src/main/java/br/com/ridgue/ridpad/controller/notes/NotesController.java
@@ -2,6 +2,7 @@ package br.com.ridgue.ridpad.controller.notes;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +16,15 @@ public class NotesController {
     @GetMapping("details")
     public String details() {
         return "notes/details";
+    }
+
+    @GetMapping("alter")
+    public String alter() {
+        return "notes/alter";
+    }
+
+    @PostMapping("altered")
+    public String altered() {
+        return "redirect:/notes/my_notes";
     }
 }
