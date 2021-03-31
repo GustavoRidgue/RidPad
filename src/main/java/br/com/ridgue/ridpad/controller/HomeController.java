8ef83872ae8e2e36a0af0616ddde13fdffcbcc34
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-31 11:23:15 -0300

    Create home page

--- src/main/java/br/com/ridgue/ridpad/controller/HomeController.java
+++ src/main/java/br/com/ridgue/ridpad/controller/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/notes")
-public class NotesController {
+@RequestMapping("/home")
+public class HomeController {
     @GetMapping()
-    public String notes() {
-        return "notes";
+    public String home() {
+        return "home";
     }
 }
