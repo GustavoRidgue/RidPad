c0b4e3c0457a0eea81d510e3dba833c4c6360017
Gustavo Ridgue <guferrari007@gmail.com>
2021-03-31 12:25:06 -0300

    Create notes page

---
+++ src/main/java/br/com/ridgue/ridpad/controller/notes/NotesController.java
@@ -0,0 +1,14 @@
+package br.com.ridgue.ridpad.controller.notes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("notes/my_notes")
+public class NotesController {
+    @GetMapping
+    public String myNotes() {
+        return "notes/mynotes";
+    }
+}
